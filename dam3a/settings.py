2a674f7cd81b533067f2399715a6010b4ca6d598
MonjedNidal <maswadehmonjed@gmail.com>
2025-01-01 17:22:48 +0200

    commit

--- dam3a/settings.py
+++ dam3a/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']  # Directory where you store your static files
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory where static files will be collected
+STATIC_ROOT = '/home/almalsxs/public_html/dam3a.pro/static'  # Directory where static files will be collected
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
